76ed5f6d7c0290227c37b97e3b4cf80e2a882eb7
JosephKorivi <joseph.korivi0911@gmail.com>
2025-05-02 00:47:53 +0530

    add functionality to update the count of the products in the cart page

--- src/main/java/com/example/jspcommerce/servlet/QuantityIncDecServlet.java
+++ src/main/java/com/example/jspcommerce/servlet/QuantityIncDecServlet.java
@@ -6,6 +6,8 @@ import jakarta.servlet.annotation.WebServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import com.example.jspcommerce.models.Cart;
 
 @WebServlet(name = "QuantityIncDecServlet", value = "/QuantityIncDecServlet")
 public class QuantityIncDecServlet extends HttpServlet {
@@ -14,7 +16,37 @@ public class QuantityIncDecServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("Inc Dec Servlet");
+        try (PrintWriter out = response.getWriter()) {
+            String action = request.getParameter("action");
+            int id = Integer.parseInt(request.getParameter("id"));
+            ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+
+            if (action != null && id >= 1) {
+                if (action.equals("inc")) {
+                    for (Cart c : cart_list) {
+                        if (c.getId() == id) {
+                            int quantity = c.getQuantity();
+                            quantity++;
+                            c.setQuantity(quantity);
+                            response.sendRedirect("cart.jsp");
+                        }
+                    }
+                }
+
+                if (action.equals("dec")) {
+                    for (Cart c : cart_list) {
+                        if (c.getId() == id && c.getQuantity() > 1) {
+                            int quantity = c.getQuantity();
+                            quantity--;
+                            c.setQuantity(quantity);
+                            break;
+                        }
+                    }
+                    response.sendRedirect("cart.jsp");
+                }
+            } else {
+                response.sendRedirect("cart.jsp");
+            }
+        }
     }
 }
