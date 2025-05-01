928cd81399cc5588ab3cba884027e08f326fe1d0
JosephKorivi <joseph.korivi0911@gmail.com>
2025-05-01 15:32:52 +0530

    update total calculate

--- src/main/java/com/example/jspcommerce/servlet/AddToCartServlet.java
+++ src/main/java/com/example/jspcommerce/servlet/AddToCartServlet.java
@@ -20,26 +20,28 @@ public class AddToCartServlet extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             ArrayList<Cart> cartList = new ArrayList<>();
             int id = Integer.parseInt(request.getParameter("id"));
-            Cart cart = new Cart();
-            cart.setId(id);
-            cart.setQuantity(1);
+            Cart cm = new Cart();
+            cm.setId(id);
+            cm.setQuantity(1);
             HttpSession session = request.getSession();
             ArrayList<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list");
             if (cart_list == null) {
-                cartList.add(cart);
+                cartList.add(cm);
                 session.setAttribute("cart-list", cartList);
-                out.println("Session Created and add the list");
+                response.sendRedirect("index.jsp");
             } else {
                 cartList = cart_list;
+
                 boolean exist = false;
-                for (Cart c : cartList) {
+                for (Cart c : cart_list) {
                     if (c.getId() == id) {
                         exist = true;
-                        out.println("<h3 style='color: crimson; text-align: center;'>Item Already Exists in the Cart.<a href='cart.jsp'>Go to Cart Page</a></h3>");
+                        out.println("<h3 style='color:crimson; text-align: center'>Item Already in Cart. <a href='cart.jsp'>GO to Cart Page</a></h3>");
                     }
                 }
+
                 if (!exist) {
-                    cartList.add(cart);
+                    cartList.add(cm);
                     response.sendRedirect("index.jsp");
                 }
             }
